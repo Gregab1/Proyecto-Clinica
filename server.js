10a21ff2e7c73239d6682e338fa7ad9e1c3d13b4
GabrielGregorio <gabriel95jk@gmail.com>
2022-11-20 11:59:52 -0300

    ACTUALIZO

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { request, response } = require('express');
 const express=require('express');
 const path=require('path');
 const app=express();
-const port= process.env.port || 3000;
+const port = process.env.PORT  || 3000;
 const funcion=require('./app');
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
